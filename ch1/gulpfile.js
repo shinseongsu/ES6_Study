1af025653262383cac8d8d5d31d0d374c98be195
Shin seongsu <tlstjdtn321@naver.com>
2020-07-17 23:06:51 +0900

    gulp, babel, eslint

---
+++ ch1/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function(done) {
+
+    gulp.src(["es6/*.js", "public/es6/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
+
+    gulp.src("public/es6/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
+
+    done();
+});
